a4d77b195c2c64a1f5142146f35ac0d7e7b58eee
AbdelilahChahbouni <root@localhost-ThinkPad-T495s>
2023-10-14 13:39:40 +0100

    create model Job

---
+++ src/analysis.md
@@ -0,0 +1,10 @@
+job description:
+    - title job
+    - location
+    - salry
+    - job_nature
+    - posted date
+    - company
+    - description
+
+
--- src/jobs/models.py
+++ src/jobs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=500)
+    salary = models.IntegerField()
+    job_nature = models.CharField(max_length=500)
+    posted_date = models.DateTimeField(default=timezone.now())
+    description = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, related_name="user_jobs", on_delete=models.SET_NULL ,null=True)
+
+
+
+
+class JobDetail(models.Model):
+    pass
+
+
