06ef30427f1b3ffd49f5c47a1f807c1d0eb5318e
AbdelilahChahbouni <root@localhost-ThinkPad-T495s.(none)>
2023-10-15 13:09:50 +0100

    update job model

--- src/jobs/models.py
+++ src/jobs/models.py
@@ -5,13 +5,19 @@ from django.contrib.auth.models import User
 class Job(models.Model):
     title = models.CharField(max_length=200)
     location = models.CharField(max_length=500)
-    salary = models.IntegerField()
+    salary = models.IntegerField(default=0, blank=True)
     job_nature = models.CharField(max_length=500)
     posted_date = models.DateTimeField(default=timezone.now())
-    description = models.CharField(max_length=1000)
-    user = models.ForeignKey(User, related_name="user_jobs", on_delete=models.SET_NULL ,null=True)
+    last_date = models.DateTimeField()
+    description = models.TextField()
+    user = models.ForeignKey(User, related_name="user_jobs", on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100)
+    company_description = models.CharField(max_length=500)
+    company_website = models.CharField(max_length=200, default="")
 
 
+    def __str__(self):
+        return self.title
 
 
 class JobDetail(models.Model):
